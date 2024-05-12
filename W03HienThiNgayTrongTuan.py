1ff8bdd26bbcb33cebfa2c2a26f93856393fc782
HuynhLuuTrungCodeBoy <huynhluutrung.codeboy@gmail.com>
2024-05-12 23:58:08 +0700

    W03 hiển thị ngày  trong tuần

---
+++ W03HienThiNgayTrongTuan.py
@@ -0,0 +1,17 @@
+so=int(input("Mời nhập số nguyên từ 1 đến 7: "))
+if so==1:
+  print("Monday")
+elif so==2: 
+  print("Tuesday")
+elif so==3: 
+  print("Wednesday")
+elif so==4: 
+  print("Thursday")
+elif so==5: 
+  print("Friday")
+elif so==6: 
+  print("Saturday")
+elif so==7: 
+  print("Sunday")
+else:
+  print("Số bạn đã nhập không đúng yêu cầu.")
